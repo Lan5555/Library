2305d301c5be805fc73458a4a868134e2677d52e
lan5555 <okekejohnson24@gmail.com>
2025-01-25 19:39:56 +0100

    update

--- src/app/pages/endtime/page.tsx
+++ src/app/pages/endtime/page.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
+'use client'
 import { faBook, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {useFlutterwave, closePaymentModal} from 'flutterwave-react-v3';
